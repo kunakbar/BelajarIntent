26430c7a7eeedbcb5722a0fb4ef98d513da929df
kunakbar <60692911+kunakbar@users.noreply.github.com>
2020-10-11 13:50:09 +0700

    Update MoveWithDataActivity.kt

--- app/src/main/java/com/example/myintentapp/MoveWithDataActivity.kt
+++ app/src/main/java/com/example/myintentapp/MoveWithDataActivity.kt
@@ -7,8 +7,8 @@ import android.widget.TextView
 class MoveWithDataActivity : AppCompatActivity() {
 
     companion object {
-        const val EXTRA_AGE = "extra_age"
         const val EXTRA_NAME = "extra_name"
+        const val EXTRA_AGE = "extra_age"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,7 +18,7 @@ class MoveWithDataActivity : AppCompatActivity() {
         val tvDataReceived: TextView = findViewById(R.id.tv_data_received)
 
         val name = intent.getStringExtra(EXTRA_NAME)
-        val age = intent.getStringExtra(EXTRA_AGE, 0)
+        val age = intent.getIntExtra(EXTRA_AGE, 0)
 
         val text = "Name : $name, Your Age : $age"
         tvDataReceived.text = text
