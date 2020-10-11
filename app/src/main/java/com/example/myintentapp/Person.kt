7ab70e72a151e2cd58c85b0b91bd3e7ae3be54d2
kunakbar <60692911+kunakbar@users.noreply.github.com>
2020-10-11 13:49:40 +0700

    Update Person.kt

--- app/src/main/java/com/example/myintentapp/Person.kt
+++ app/src/main/java/com/example/myintentapp/Person.kt
@@ -1,11 +1,12 @@
 package com.example.myintentapp
 
-import android.os.Parcel
 import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
 
-data class Person {
+@Parcelize
+data class Person (
     val name: String?,
     val age: Int?,
     val email: String?,
     val city: String?
-} : Parcelable
+) : Parcelable
